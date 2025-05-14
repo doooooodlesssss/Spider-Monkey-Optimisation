46220c1fa05c1b8fbe545a8419da257d890a565f
Diya Bangera <73332927+doooooodlesssss@users.noreply.github.com>
2025-05-14 10:59:32 +0530

    Add files via upload

--- evaluations/utils.py
+++ evaluations/utils.py
@@ -1,1 +1,15 @@
+# evaluations/utils.py
 
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def save_convergence_plot(data, filename="convergence_plot.png"):
+    plt.plot(data)
+    plt.title("Convergence Plot")
+    plt.xlabel("Generations")
+    plt.ylabel("Fitness Value")
+    plt.savefig(filename)
+
+def save_results_to_csv(data, filename="mean_std.csv"):
+    df = pd.DataFrame(data)
+    df.to_csv(filename, index=False)
